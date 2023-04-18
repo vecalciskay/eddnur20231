63b34b0f8cc29143c0c12c6fe9d7aa13b1d56aba
Vladimir <vladimir.calderon@gmail.com>
2023-04-17 20:58:28 -0400

    Cambio en tonos de gris

--- src/main/java/edu/nur/edd/imagenes/gui/ImagenFrame.java
+++ src/main/java/edu/nur/edd/imagenes/gui/ImagenFrame.java
@@ -1,25 +1,71 @@
 package edu.nur.edd.imagenes.gui;
 
 import edu.nur.edd.imagenes.obj.Imagen;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class ImagenFrame extends JFrame {
+    private static Logger logger = LogManager.getRootLogger();
+    private Imagen modelo;
     public ImagenFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getContentPane().setLayout(new BorderLayout());
 
-        Imagen modelo = new Imagen(600,400);
-        //modelo.todoUnColor();
-        modelo.leer();
-        modelo.gris();
+        modelo = new Imagen(600,400);
         ImagenPanel panel = new ImagenPanel(modelo);
-
+        modelo.addObserver(panel);
         this.getContentPane().add(panel, BorderLayout.CENTER);
+
+        menu();
 
         this.pack();
         this.setVisible(true);
+    }
+
+    private void menu() {
+        JMenuBar bar = new JMenuBar();
+
+        JMenu menu = new JMenu("Archivo");
+        JMenuItem item = new JMenuItem("Abrir");
+        item.addActionListener(e -> {
+            menuArchivoItemAbrir();
+        });
+        menu.add(item);
+
+        menu.addSeparator();
+
+        item = new JMenuItem("Salir");
+        item.addActionListener(e -> {
+            logger.info("El usuario sale del programa");
+            System.exit(0);
+        });
+
+        bar.add(menu);
+
+        menu = new JMenu("Imagen");
+        item = new JMenuItem("Gris");
+        item.addActionListener(e -> {
+            logger.info("El usuario convierte la imagen a escala de grises");
+            modelo.gris();
+        });
+        menu.add(item);
+
+        bar.add(menu);
+
+        this.setJMenuBar(bar);
+    }
+
+    private void menuArchivoItemAbrir() {
+        logger.info("Abrimos el file chooser para escoger un archivo");
+        JFileChooser chooser = new JFileChooser();
+        int result = chooser.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            logger.info(chooser.getSelectedFile().getAbsolutePath());
+            modelo.leer(chooser.getSelectedFile());
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/edu/nur/edd/imagenes/obj/Imagen.java
+++ src/main/java/edu/nur/edd/imagenes/obj/Imagen.java
@@ -1,8 +1,14 @@
 package edu.nur.edd.imagenes.obj;
 
+import edu.nur.edd.imagenes.gui.ImagenPanel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.File;
 import java.io.IOException;
 
@@ -11,10 +17,15 @@ public class Imagen {
     private int alto;
     private int[][] pixeles;
 
+    private PropertyChangeSupport observado;
+    private static Logger logger = LogManager.getRootLogger();
+
     public Imagen(int w, int h) {
         ancho = w;
         alto = h;
         pixeles = new int[w][h];
+        observado = new PropertyChangeSupport(this);
+        logger.info("Se crea una imagen vacia de " + w + "x" + h);
     }
 
     public void dibujar(Graphics g) {
@@ -35,25 +46,11 @@ public class Imagen {
     public int getAncho() {
         return ancho;
     }
-
-    public void setAncho(int ancho) {
-        this.ancho = ancho;
-    }
-
     public int getAlto() {
         return alto;
     }
-
-    public void setAlto(int alto) {
-        this.alto = alto;
-    }
-
     public int[][] getPixeles() {
         return pixeles;
-    }
-
-    public void setPixeles(int[][] pixeles) {
-        this.pixeles = pixeles;
     }
 
     public void todoUnColor() {
@@ -62,6 +59,7 @@ public class Imagen {
                 pixeles[i][j] = 0xFFFFFF;
             }
         }
+        observado.firePropertyChange("IMAGEN", false, true);
     }
 
     public void bandera() {
@@ -76,12 +74,12 @@ public class Imagen {
                 pixeles[i][j] = 0x32CD32;
             }
         }
+        observado.firePropertyChange("IMAGEN", false, true);
     }
 
-    public void leer() {
+    public void leer(File f) {
         BufferedImage bi = null;
         try {
-            File f = new File("C:\\temp\\primero.jpg");
             bi = ImageIO.read(f);
         } catch (IOException e) {
             e.printStackTrace();
@@ -97,6 +95,8 @@ public class Imagen {
                 pixeles[i][j] = bi.getRGB(i,j);
             }
         }
+
+        observado.firePropertyChange("IMAGEN", false, true);
     }
 
     public void gris() {
@@ -106,8 +106,14 @@ public class Imagen {
                 int g = (pixeles[i][j] >> 8) & 0x0000FF;
                 int b = pixeles[i][j] & 0x0000FF;
                 int prom = (r+g+b)/3;
-                pixeles[i][j] = prom | (prom << 8) | (prom << 16);
+
+                pixeles[i][j] = prom + prom * 256 + prom * 256 * 256;
             }
         }
+        observado.firePropertyChange("IMAGEN", false, true);
+    }
+
+    public void addObserver(PropertyChangeListener listener) {
+        observado.addPropertyChangeListener(listener);
     }
 }
